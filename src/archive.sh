d1e4c5a0b5e8d79147a63ce991ccbead86ca24f8
Grama Nicolae <gramanicu@gmail.com>
2019-11-07 14:50:56 +0200

    Modified archive creation for windows (needs the executable)

--- src/archive.sh
+++ src/archive.sh
@@ -2,4 +2,9 @@
 AFLAGS=-FSr
 ANAME=322CA_GramaNicolae_Tema1.zip
 ACONTENTS="Readme.md com/"
-zip $AFLAGS $ANAME $ACONTENTS
+if zip $AFLAGS $ANAME $ACONTENTS ; then
+    echo "Archive created"
+else
+    ./zip $AFLAGS $ANAME $ACONTENTS
+    echo "Archive created"
+fi
